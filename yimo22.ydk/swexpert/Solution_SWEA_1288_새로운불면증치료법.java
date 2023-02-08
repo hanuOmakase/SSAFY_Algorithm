baa04e741be0a406f8e9709ddf7c3decb80c47af
goal961 <goal961@naver.com>
2023-02-08 15:14:35 +0900

    [SWEA] 실습내용

---
+++ yimo22.ydk/swexpert/Solution_SWEA_1288_새로운불면증치료법.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_SWEA_1288_새로운불면증치료법 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+	}
+}
